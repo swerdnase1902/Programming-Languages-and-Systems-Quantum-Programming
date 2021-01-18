13a477b3e9c220f35cf8c4444f58044e1dc27748
zucchini <zucchini@zucchini.local>
2021-01-18 12:24:37 -0800

    completed coding assignment 2: 1/18 12:24

--- week3/coding assignment/Bernstein-Vazirani.py
+++ week3/coding assignment/Bernstein-Vazirani.py
@@ -3,11 +3,32 @@ def bernstein_vazirani(f, n):
     a = 0
     input_to_f = 1
     for shift in range(n):
-        a <<= 1
-        a_bit = (f(input_to_f)) ^ b
+        a_bit = (f(input_to_f)) ^ b  # Since f = a*x + b, we have f + b = a*x
+        a_bit <<= shift
         input_to_f <<= 1
         a = a | a_bit
     return a, b
 
+
 if __name__ == '__main__':
-    pass
+    def f1(input):
+        a = 0b000101  # a = 5
+        b = 0b1  # b = 1
+        return (bin(a & input).count("1") & 1) ^ b  # performs a*x + b
+
+
+    def f2(input):
+        a = 0b111011
+        b = 0b0
+        return (bin(a & input).count("1") & 1) ^ b
+
+
+    print('About to test bernstein_vazirani on f1 (a=0b000101, b=1)...')
+    result_f1 = bernstein_vazirani(f1, 6)
+    expected_f1 = (0b000101, 0b1)
+    print("bernstein_vazirani works for f1 (a=0b000101, b=1)? {}".format(result_f1 == expected_f1))
+
+    print('About to test bernstein_vazirani on f2 (a=0b111011, b=0)...')
+    result_f2 = bernstein_vazirani(f2, 6)
+    expected_f2 = (0b111011, 0b0)
+    print("bernstein_vazirani works for f2 (a=0b111011, b=0)? {}".format(result_f2 == expected_f2))
--- week3/coding assignment/Deutsch-Jozsa.py
+++ week3/coding assignment/Deutsch-Jozsa.py
@@ -17,8 +17,13 @@ if __name__ == '__main__':
     def f2(input):
         return (input & 1)  # returns lowest bit of input => balanced
 
-
+    print('About to test deutsch_jozsa on f1, which is a constant function that always returns 0')
     result_f1 = deutsch_jozsa(f1, 5)
-    result_f2 = deutsch_jozsa(f2, 7)
+    expected_f1 = 1
+    print('deutsch_jozsa works for f1? {}'.format(result_f1 == expected_f1))
 
-    exit(0)
+    print('About to test deutsch_jozsa on f2, which is a balanced function that returns the lowest bit')
+    result_f2 = deutsch_jozsa(f2, 7)
+    expected_f2 = 0
+    print('deutsch_jozsa works for f2? {}'.format(result_f2 == expected_f2))
+    
