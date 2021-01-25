a9d3d615c15f25e6c22d4b3826081cf2810d6980
Your Name <you@example.com>
2021-01-25 13:11:43 -0800

    coding assignment 3 complete: 1/25 13:11

--- week4/coding assignment/grover.py
+++ week4/coding assignment/grover.py
@@ -1,9 +1,29 @@
 def grover(f, n):
+    # loop through all possible inputs and see if output is 1
     num_tries = 2 ** n
     for f_input in range(num_tries):
         if f(f_input) == 1:
             return 1
     return 0
 
+
 if __name__ == '__main__':
-    pass
+    def f1(x):
+        return 0
+
+
+    def f2(x):
+        return int(x == 0b1011)
+
+
+    print('About to test classical grover solver on f1 which always outputs 0')
+    f1_expected = 0
+    f1_calculated = grover(f1, 4)
+    print('Expected to get {} and actually got {}'.format(f1_expected, f1_calculated))
+    print('Did the classical solver get the correct answer? {}'.format(f1_expected == f1_calculated))
+
+    print('About to test classical grover solver on f2 which outputs 1 only when x=0b1011')
+    f2_expected = 1
+    f2_calculated = grover(f2, 4)
+    print('Expected to get {} and actually got {}'.format(f2_expected, f2_calculated))
+    print('Did the classical solver get the correct answer? {}'.format(f2_expected == f2_calculated))
--- week4/coding assignment/simon.py
+++ week4/coding assignment/simon.py
@@ -1,14 +1,27 @@
 def simon(f, n):
     # n is bit length
-    num_tries = 2**n
-    output_to_input = [None]*num_tries  # the index is f(x) and the value is x
+    num_tries = 2 ** n
+    # We use the output_to_input table to check if a specific output of f(x) has been seen before...
+    output_to_input = [None] * num_tries  # the index is f(x) and the value is x
     for f_input in range(num_tries):
         f_output = f(f_input)
         if output_to_input[f_output] is not None:
+            # Here, we found that f_output has been observed before
             s = f_input ^ output_to_input[f_output]
             return s
         output_to_input[f_output] = f_input
     return None
 
+
 if __name__ == '__main__':
-    pass
+    def f1(x):
+        # s = 0b101
+        table = [0b000, 0b010, 0b001, 0b100, 0b010, 0b000, 0b100, 0b001]
+        return table[x]
+
+
+    print('About to test classical Simon solver on f1 with s=101')
+    f1_expected_s = 0b101
+    f1_calculated_s = simon(f1, 3)
+    print('Expected to get s={} and actually got s={}'.format(bin(f1_expected_s), bin(f1_calculated_s)))
+    print('Did the classical solver get the correct s? {}'.format(f1_expected_s == f1_calculated_s))
